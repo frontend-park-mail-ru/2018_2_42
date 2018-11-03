d136becb6b0c0604817ca2a68873353d0564dfca
Koretsky Mikhail <koretsky97@gmail.com>
2018-11-03 17:34:31 +0300

    html scene, css animations

---
+++ animation.js
@@ -0,0 +1,18 @@
+function resizeUnits(event) {
+    var td = document.getElementsByTagName('td')[0];
+
+    var units = document.getElementsByClassName('unit');
+    console.log(units.array)
+    Array.prototype.forEach.call(units, function (element) {
+        console.log(td.clientHeight)
+        element.style.transform = 'scale(calc(' + td.clientHeight + '/ 63)'
+    });
+
+    var traps = document.getElementsByClassName('animate-trap-open');
+    Array.prototype.forEach.call(traps, function (element) {
+        element.style.transform = 'scale(calc(' + td.clientHeight + '/ 63)'
+    });
+};
+
+window.onresize = resizeUnits;
+window.onload = resizeUnits;
