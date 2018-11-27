915871f923194c8c51a3e4dcb859b6ed0af21633
Koretsky Mikhail <koretsky97@gmail.com>
2018-11-27 17:34:13 +0300

    singleplayer without bot

--- public/js/modules/game/core/teams.js
+++ public/js/modules/game/core/teams.js
@@ -1,4 +1,4 @@
 export default {
-    RED: 'RED',
-    BLUE: 'BLUE'
+    RED: 'red',
+    BLUE: 'blue'
 }
--- public/js/modules/game/game.js
+++ public/js/modules/game/game.js
@@ -1,27 +1,28 @@
 'use strict';
 import OnlineGame from "./core/online.js";
 import OfflineGame from "./core/offline.js";
-// import GameScene from "./core/offline.js";
+import GameScene from "./scene.js";
+import GameController from "./controller.js";
 
 export default class Game {
-    constructor({ mode = "offline" }) {
+    constructor({ mode = "offline", gameField = document.getElementsByClassName("game")[0] }) {
+        this.gameField = gameField;
         let GameConstructor = null;
         switch (mode) {
             case "offline": {
-                GameConstructor = OnlineGame;
+                GameConstructor = OfflineGame;
                 break;
             }
             case "online": {
-                GameConstructor = OfflineGame;
+                GameConstructor = OnlineGame;
                 break;
             }
             default:
                 throw new Error('Invalid game mode ' + mode);
         }
 
-        // this.gameScene = new GameScene();
-        this.gameScene = null;
-
+        this.gameScene = new GameScene(this.gameField);
+        this.gameController = new GameController(this.gameField);
         this.gameCore = new GameConstructor({ scene: this.gameScene });
     }
 
