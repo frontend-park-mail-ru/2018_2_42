5b7743966b3eef763aa83d841509e7b65077e190
Koretsky Mikhail <koretsky97@gmail.com>
2018-12-14 17:22:52 +0300

    webpack zopfli

--- public/js/components/About/About.mjs
+++ public/js/components/About/About.mjs
@@ -1,6 +1,11 @@
 'use strict'
 
-import tmpl from "./About.tmpl.xml"
+import tmpl from './About.tmpl.xml';
+import andrey from './images/andrey.jpg';
+import misha from './images/misha.jpg';
+import daniel from './images/daniel.jpg';
+import maxim from './images/maxim.jpg';
+import oleg from './images/oleg.jpg';
 
 export default class AboutComponent {
     constructor({ el = document.body } = {}) {
@@ -10,11 +15,11 @@ export default class AboutComponent {
     render() {
         const data = {
             contributors: [
-                { name: 'Дикарев Андрей', gitHubUrl: 'https://github.com/DikarevAndrey', position: "Frontend", avatar: "../../images/about/andrey.jpg" },
-                { name: 'Корецкий Михаил', gitHubUrl: 'https://github.com/koretskyhub', position: "Backend", avatar: "../../images/about/misha.jpg" },
-                { name: 'Ли Даниил', gitHubUrl: 'https://github.com/Unanoc', position: "Frontend", avatar: "../../images/about/daniel.jpg" },
-                { name: 'Семёнов Максим', gitHubUrl: 'https://github.com/OlegSchwann', position: "Backend", avatar: "../../images/about/maxim.jpg" },
-                { name: 'Морозенков Олег', gitHubUrl: 'https://github.com/reo7sp', position: "Лучший ментор", avatar: "../../images/about/oleg.jpg" }
+                { name: 'Дикарев Андрей', gitHubUrl: 'https://github.com/DikarevAndrey', position: "Frontend", avatar: andrey },
+                { name: 'Корецкий Михаил', gitHubUrl: 'https://github.com/koretskyhub', position: "Backend", avatar: misha },
+                { name: 'Ли Даниил', gitHubUrl: 'https://github.com/Unanoc', position: "Frontend", avatar: daniel },
+                { name: 'Семёнов Максим', gitHubUrl: 'https://github.com/OlegSchwann', position: "Backend", avatar: maxim },
+                { name: 'Морозенков Олег', gitHubUrl: 'https://github.com/reo7sp', position: "Лучший ментор", avatar: oleg }
             ],
             fronendtUrl: {
                 caption: "Frontend",
