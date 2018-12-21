bfbe14907862a2d6201560ec0979697568e7ce09
Unanoc <leedaniil98@gmail.com>
2018-12-21 19:28:53 +0300

    bugs commented

--- public/js/modules/game/scene.js
+++ public/js/modules/game/scene.js
@@ -51,10 +51,10 @@ export default class GameScene {
 		indicatorClasses.remove('red-turn', 'blue-turn');
 		switch (clr){
 		case TEAMS.BLUE: indicatorClasses.add('blue-turn');
-			window.bus.publish('change-turn', 'blue')
+			// window.bus.publish('change-turn', 'blue')
 			break;
 		case TEAMS.RED: indicatorClasses.add('red-turn');
-			window.bus.publish('change-turn', 'red')
+			// window.bus.publish('change-turn', 'red')
 			break;
 		default: throw 'incorrect color';
 		}
