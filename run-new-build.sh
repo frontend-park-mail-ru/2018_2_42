2cc322103cafd68d308c11537e02dc7fb0d35479
Oleg Morozenkov <m@oleg.rocks>
2018-12-29 01:25:18 +0300

    [*] [infra] fix docker

--- run-new-build.sh
+++ run-new-build.sh
@@ -3,9 +3,8 @@ sudo docker stop frontend
 sudo docker run \
     --name 'frontend' \
     --publish 10080:80 \
-    --publish 100443:443 \
+    --publish 10443:443 \
     --network 'rpsarena-net' \
     --volume '/etc/letsencrypt':'/etc/letsencrypt' \
-    --volume '/home/ubuntu/presentation':'/var/www/presentation' \
     --detach \
     --rm koretskyhub/front:nginx
